941220cc9a4d59fd1ea0f8c3fe68ad744c17f9be
thicco-catto <cristobal.martin01@gmail.com>
2023-11-03 17:54:19 +0100

    feat: Added delete operation for subastas endpoint

--- src/app/api/subastas/route.ts
+++ src/app/api/subastas/route.ts
@@ -28,6 +28,16 @@ export async function GET(request: NextRequest) {
         res = await subastas.find({
             _id: {$eq: ObjectId.createFromHexString(id)}
         }).toArray();
+
+        // Do we only need to send 404 if they specify the id?
+        if(res.length === 0) {
+            return NextResponse.json(
+                {},
+                {
+                    status: 404
+                }
+            );
+        }
     } else {
         res = await subastas.find().limit(20).toArray();
     }
@@ -53,7 +63,6 @@ export async function POST(request: NextRequest) {
         );
     }
 
-    
     const result = await subastas.insertOne(json);
 
     const status = result.acknowledged? 200: 500;
@@ -63,6 +72,37 @@ export async function POST(request: NextRequest) {
         {
             id: id
         },
+        {
+            status: status
+        }
+    );
+}
+
+export async function DELETE(request: NextRequest) {
+    const params = request.nextUrl.searchParams;
+    const id = params.get("id");
+    
+    if(!id) {
+        return NextResponse.json(
+            {
+                message: "You need to specify the id of the element to be deleted."
+            },
+            {
+                status: 400
+            }
+        );
+    }
+
+    const subastas = await GetSubastas();
+
+    const res = await subastas.deleteOne({
+        _id: {"$eq": ObjectId.createFromHexString(id)}
+    });
+
+    const status = res.acknowledged ? 200: 500;
+
+    return NextResponse.json(
+        {},
         {
             status: status
         }
