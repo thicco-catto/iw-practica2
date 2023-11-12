e6d6f7a40023557270ffd007056567a97dc9f4fa
thicco-catto <cristobal.martin01@gmail.com>
2023-11-12 10:42:05 +0100

    chore: Cleaned up calculate route

--- src/app/api/mapa/huellaCarbono/calculate/route.ts
+++ src/app/api/mapa/huellaCarbono/calculate/route.ts
@@ -1,6 +1,5 @@
 import { getCO2Footprint } from "@/lib/carbonInterface";
 import { HasAllKeys } from "@/lib/dict_helper";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import { NextRequest, NextResponse } from "next/server";
 
 const KEYS: string[] = [
@@ -12,21 +11,23 @@ const KEYS: string[] = [
 
 export async function POST(request: NextRequest){
     const json = await request.json();
-    console.log ("Hola");
+    
     if(!HasAllKeys(json, KEYS)) {
         return NextResponse.json({},
             {
                 status: 406
             }
         );
-    };
+    }
     const adressFrom = json.adressFrom;
     const adressTo = json.adressTo;
     const model = json.model;
-    const response = await fetch("http://localhost:3000/api/mapa/distancia?from=" 
-    + adressFrom + 
-    "&to="+adressTo,{method:'GET'} );
-    const distancejson = await response.json()
+    const response = await fetch(
+        `http://localhost:3000/api/mapa/distancia?from=${adressFrom}&to=${adressTo}`,
+        {method: "GET"}
+    );
+
+    const distancejson = await response.json();
     const distance = distancejson.distance;
 
 
