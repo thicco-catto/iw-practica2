d0a67aa99ada39f171f2655f09a10d78d9f96ab3
thicco-catto <cristobal.martin01@gmail.com>
2023-11-12 10:52:19 +0100

    chore: More cleaning

--- src/app/api/mapa/huellaCarbono/calculate/route.ts
+++ src/app/api/mapa/huellaCarbono/calculate/route.ts
@@ -1,6 +1,10 @@
 import { getCO2Footprint } from "@/lib/carbonInterface";
 import { HasAllKeys } from "@/lib/dict_helper";
 import { NextRequest, NextResponse } from "next/server";
+import { GetDistance } from "../../distancia/route";
+import { GetDirecciones } from "@/lib/database";
+import { GetIdFilter } from "@/lib/route_helper";
+import { ObjectId } from "mongodb";
 
 const KEYS: string[] = [
     "adressFrom",
@@ -11,7 +15,7 @@ const KEYS: string[] = [
 
 export async function POST(request: NextRequest){
     const json = await request.json();
-    
+
     if(!HasAllKeys(json, KEYS)) {
         return NextResponse.json({},
             {
@@ -21,21 +25,41 @@ export async function POST(request: NextRequest){
     }
     const adressFrom = json.adressFrom;
     const adressTo = json.adressTo;
-    const model = json.model;
-    const response = await fetch(
-        `http://localhost:3000/api/mapa/distancia?from=${adressFrom}&to=${adressTo}`,
-        {method: "GET"}
-    );
 
-    const distancejson = await response.json();
-    const distance = distancejson.distance;
+    if(!ObjectId.isValid(adressFrom) || !ObjectId.isValid(adressTo)) {
+        return NextResponse.json(
+            {
+                msg: "The object ids are not correct."
+            },
+            {
+                status: 406
+            }
+        );
+    }
 
+    const direcciones = await GetDirecciones();
 
-    const result = await getCO2Footprint(parseFloat(distance), model);
+    const from = await direcciones.findOne(GetIdFilter(adressFrom));
+    const to = await direcciones.findOne(GetIdFilter(adressTo));
+
+    if(!from || !to) {
+        return NextResponse.json(
+            {
+
+            },
+            {
+                status: 404
+            }
+        );
+    }
+
+    //const model = json.model;
+    const distance = await GetDistance(from, to);
+
+    const result = await getCO2Footprint(distance);
     if(!result){
         return NextResponse.json({Error: "Internal Error"},{status: 500});
     }else{
         return NextResponse.json(result,{status: 200});
     }
-
 }
--- src/lib/carbonInterface.ts
+++ src/lib/carbonInterface.ts
@@ -31,15 +31,22 @@ export async function GetVehiclesModels(id: string) {
 
 }
 
-export async function getCO2Footprint(distance: Number , model: string){
-    const response = await fetch("https://www.carboninterface.com/api/v1/estimates", {mode:'cors', method: 'POST', 
-    headers:{'Autorization': authenticationKey},body: JSON.stringify({
-    type: 'vehicle', 
-    distance_unit: 'km', 
-    distance_value: 100, 
-    vehicle_model_id: '7268a9b7-17e8-4c8d-acca-57059252afe9'})})
+export async function getCO2Footprint(distance: number) {
+    const response = await fetch(
+        "https://www.carboninterface.com/api/v1/estimates",
+        {
+            mode: "cors",
+            method: "POST",
+            headers: {
+                "Autorization": authenticationKey
+            },
+            body: JSON.stringify({
+                type: "vehicle",
+                distance_unit: "km",
+                distance_value: distance,
+                vehicle_model_id: "7268a9b7-17e8-4c8d-acca-57059252afe9"
+            })
+        }
+    );
     return response.json();
-
-
-
 }
